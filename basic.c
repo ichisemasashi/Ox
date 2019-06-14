276f2a957053eb8cd8de6a0e9b683c0edd330f2a
ichisemasashi <ichise.masashi@gmail.com>
2019-06-14 23:59:37 +0900

    basic-lisp start...

--- basic.c
+++ basic.c
@@ -1,8 +1,129 @@
 #include <stdio.h>
+#include <string.h>
 #include "basic.h"
 
+void init() {
+  int size;
+  size = sizeof(cell_BUF);
+  memset(cell_BUF, 0x00, size);
+  size = sizeof(Cons_Cell_BUF);
+  memset(Cons_Cell_BUF, 0x00, size);
+
+  size = sizeof(sym_BUF);
+  memset(sym_BUF, 0x00, size);
+  size = sizeof(str_BUF);
+  memset(str_BUF, 0x00, size);
+  size = sizeof(int_BUF);
+  memset(int_BUF, 0x00, size);
+  size = sizeof(float_BUF);
+  memset(float_BUF, 0x00, size);
+}
+
+void free_cell(struct cell *p) {
+  p->gc = not_use;
+  p->kind = empty;
+  p->value = NULL;
+}
+void free_cons(struct cons_cell *p) {
+  p->gc = not_use;
+  p->car = NULL;
+  p->cdr = NULL;
+}
+
+void free_cons_perfect(struct cons_cell *p) {
+  if (p->car != NULL) {
+  }
+
+  if (p->cdr != NULL) {
+
+  }
+
+  free_cons(p);
+
+void free_symbol(i_symbol *);
+void free_strings(i_strings *);
+void free_int(i_int *);
+void free_float(i_float *);
+int get_cell(struct cell *p){
+  int ret = NG, i;
+
+  for (i=0; i<Cell_BUF_SIZE; i++) {
+    if (cell_BUF[i].gc == not_use) {
+      p = &(cell_BUF[i]);
+      ret = OK;
+      break;
+    }
+  }
+
+  return ret;
+}
+int get_cons_cell(struct cons_cell *p) {
+  int i, ret = NG;
+
+  for (i=0; i<Cons_Cell_BUF_SIZE; i++) {
+    if (Cons_Cell_BUF[i].gc == not_use) {
+      p = &(Cons_Cell_BUF[i]);
+      ret = OK;
+      break;
+    }
+  }
+
+  return ret;
+}
+int get_symbol(i_symbol *);
+int get_strings(i_strings *);
+int get_int(i_int *);
+int get_float(i_float *);
+
+void prompter(char *str) {printf("%s ",str);}
+int read_from(FILE *f, unsigned char *out, int sz) {
+  int paren = 0, c, ret = OK, i = 0;
+
+  while((c = getc(f)) != EOF) {
+    if (c == '\n') {
+      if (paren == 0) {
+        ret = OK;
+        break;
+      }
+      continue;
+    }
+
+    if (c == '(') {
+      paren++;
+    } else if (c == ')') {
+      paren--;
+    }
+    if (sz <= i) {
+      ret = NG;
+      break;
+    }
+    out[i] = (unsigned char)c;
+    i++;
+  }
+
+  if (ret == OK) {
+    out[i] = 0x00;
+  }
+  return ret;
+}
+int parse_input(unsigned char *in, int sz) {
+  int ret = OK;
+  return ret;
+}
+
 int my_read() {
+  int ret, size;
+  unsigned char InputBuf[4096];
+
+  size = sizeof(InputBuf)/sizeof(unsigned char);
+  prompter("Lisp >");
+  ret = read_from (stdin, InputBuf, size);
+  if (ret == NG) {
+    return NG;
+  }
+  ret = parse_input(InputBuf, size);
   return OK;
+
 }
 int my_eval() {
   return OK;
@@ -14,6 +135,7 @@ int my_print() {
 int main () {
     int ret;
 
+    init();
     while(1) {
         ret = my_read();
         if(ret == NG) {
--- basic.h
+++ basic.h
@@ -1,6 +1,62 @@
 #define OK 0
 #define NG 1
 
+typedef enum {not_use = 0, use} gc_type;
+typedef enum {empty = 0, NIL, SYMBOL, STRINGS, INT, FLOAT, CONS_CELL} kind;
+struct cell {
+  gc_type gc;
+  kind kind;
+  void *value;
+};
+#define Inner_BUF_SIZE 4096
+#define Cell_BUF_SIZE Inner_BUF_SIZE
+struct cell cell_BUF[Cell_BUF_SIZE];
+
+struct cons_cell {
+  gc_type gc;
+  struct cell *car;
+  struct cons_cell *cdr;
+};
+#define Cons_Cell_BUF_SIZE Inner_BUF_SIZE
+struct cons_cell Cons_Cell_BUF[Cons_Cell_BUF_SIZE];
+
+typedef struct {
+  gc_type gc;
+  unsigned char str[256];
+} i_symbol;
+typedef i_symbol i_strings;
+typedef struct {
+  gc_type gc;
+  int i;
+} i_int;
+typedef struct {
+  gc_type gc;
+  double f;
+} i_float;
+
+i_symbol sym_BUF[Inner_BUF_SIZE];
+i_strings str_BUF[Inner_BUF_SIZE];
+i_int int_BUF[Inner_BUF_SIZE];
+i_float float_BUF[Inner_BUF_SIZE];
+
+void init();
+void free_cell(struct cell *);
+void free_cons(struct cons_cell *);
+void free_cons_perfect(struct cons_cell *);
+void free_symbol(i_symbol *);
+void free_strings(i_strings *);
+void free_int(i_int *);
+void free_float(i_float *);
+int get_cell(struct cell *);
+int get_cons_cell(struct cons_cell *);
+int get_symbol(i_symbol *);
+int get_strings(i_strings *);
+int get_int(i_int *);
+int get_float(i_float *);
+
 int my_read();
 int my_eval();
 int my_print();
+void prompter(char *);
+int read_from(FILE *, unsigned char *, int);
+int parse_input(unsigned char *, int);
