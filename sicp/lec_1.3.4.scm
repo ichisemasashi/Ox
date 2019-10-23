f1e9da80029feb9189e4dc0040008fce4cf96114
ichise.masashi <ichise.masashi@gmail.com>
2019-10-23 12:44:31 +0900

    [WIP] segmentation fault occur

--- sicp/lec_1.3.4.scm
+++ sicp/lec_1.3.4.scm
@@ -4,4 +4,17 @@
   (lambda (x) (average x (f x)))))
 ((average-damp square) 10)
 ; 期待値は55
-
+(define sqrt (lambda (x)
+  (fixed-point (average-damp (lambda (y) (/ x y))) 1.0)))
+(define fixed-point (lambda (f first-guess)
+  (let ((try (lambda (guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))))
+  (try first-guess))))
+(define close-enough? (lambda (v1 v2)
+    (< (abs (- v1 v2)) tolerance)))
+(define tolerance 0.00001)
+(sqrt 81.0)
+; 9
